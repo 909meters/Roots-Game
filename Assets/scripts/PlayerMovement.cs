f09a54c68295ea356bcfb691d316629850529cde
Strawleka <alina-5_2001@mail.ru>
2023-02-05 15:55:09 +0600

    With background

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
+    [SerializeField] private LayerMask jumpableGround;
     private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 12f;
+    private enum MovementState { idle, running, cry, angry}
 
     
 
@@ -15,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -23,34 +29,41 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal"); //Raw позволяет не скользить
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y); //Умножаем чтобы ходил и налево если направление х отриц
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y); //Умножаем чтобы ходил и налево если направление х отриц
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, 12f);  //7=высота на которую прыгаешь
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);  //7=высота на которую прыгаешь
         }
 
         UpdateAnimationUpdate();
 
-        
     }
 
     private void UpdateAnimationUpdate()
     {
+        MovementState state;
         if(dirX > 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("running", true);
+            state = MovementState.running;      
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("running", false);
+            state = MovementState.idle;
         }
+
+        anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 
 }
